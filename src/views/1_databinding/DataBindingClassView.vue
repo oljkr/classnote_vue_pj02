a57f8c45454f6682e07c9ef19ca74aa9e1a14acd
Eunju Jeong <zaoljkr@gmail.com>
2023-01-02 12:30:08 +0900

    스타일바인딩까지

---
+++ src/views/1_databinding/DataBindingClassView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div :class="{'text-red':true}">클래스 바인딩1</div>
+    <div :class="{'text-red':false}">클래스 바인딩2</div>
+    <div :class="{'text-red':true, 'active':true }">클래스 바인딩3</div>
+    <div :class="{'text-red':true, active:true }">클래스 바인딩4</div>
+    <!--성택자명이 두개의 단어로 구성되어 있으면 ' 기호 생략시 에러
+    <div :class="{ text-red:true }">클래스 바인딩4</div>
+    -->
+    <div :class="{'text-red':hasError, 'active':isActive }">클래스 바인딩5</div>
+    <div :class="class2">클래스 바인딩6</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      hasError: true,
+      isActive: true,
+      class2: ['active', 'text-red']
+    }
+  }
+}
+</script>
+
+<style scoped>
+.active { background-color: greenyellow; font-weight: bold; }
+.text-red { color: red; }
+</style>
